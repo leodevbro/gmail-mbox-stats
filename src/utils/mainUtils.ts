5ac034f30b25deb5cbc015ca3244c47e86a31aff
Levan Katsadze <leodevbro@gmail.com>
2024-02-21 00:33:27 +0400

    create getEnvItemValue function

---
+++ src/utils/mainUtils.ts
@@ -0,0 +1,45 @@
+/*
+
+process.argv looks liek this:
+
+[
+  'C:\\Program Files\\nodejs\\node.exe',
+  'C:\\Users\\leodevbroUser\\AppData\\Local\\npm-cache\\_npx\\6b3a38aa2be73d0e\\node_modules\\gmail-mbox-stats\\bin.js',
+  'enbbbb=  qqq   kkkk'
+]
+
+*/
+
+export const getEnvItemValue = (name: string) => {
+  const allEnvItems = process.argv;
+
+  for (const item of allEnvItems) {
+    const nameAndVal = item.split("=");
+    const theKey = nameAndVal[0];
+    if (theKey === name) {
+      // we have the key. now let's find value.
+
+      if (nameAndVal.length === 1) {
+        return true;
+      }
+
+      if (nameAndVal.length !== 2) {
+        throw new Error(
+          `argument notation probably is not correct >>>${item}<<<`,
+        );
+      }
+
+      // now, nameAndVal.length is exactly 2
+      const theValue = nameAndVal[1];
+      if (theValue && typeof theValue === "string") {
+        return theValue;
+      } else {
+        throw new Error(
+          `argument notation probably is not correct >>>${item}<<<`,
+        );
+      }
+    }
+  }
+
+  return undefined;
+};
