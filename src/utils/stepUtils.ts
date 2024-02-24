da6b90adb49fe8b1c86121edb82b2abb2a35e661
Levan Katsadze <leodevbro@gmail.com>
2024-02-24 15:07:54 +0400

    update step utils file

--- src/utils/stepUtils.ts
+++ src/utils/stepUtils.ts
@@ -14,72 +14,80 @@ const str_FullInfo = "FullInfo";
 export type TyOneFileIndicator = {
   fileName: `${string}.csv`;
   pathAbsOrRel: string;
+  freqMap: Map<string, number>;
 };
 
 export type TyOneFolderIndicator = {
   folderName: string;
   pathAbsOrRel: string;
-  innerFolders: {
-    [Key: string]: TyOneFolderIndicator;
-  };
   innerFiles: {
     [Key: string]: TyOneFileIndicator;
+  };
+  innerFolders: {
+    [Key: string]: TyOneFolderIndicator;
   };
 };
 
 // export const groundFolder: TyOneFolderIndicator
 export const groundFolder = {
   folderName: "groundFolder",
-  pathAbsOrRel: "",
+  pathAbsOrRel: "" as string,
+  innerFiles: {},
   innerFolders: {
     mboxStats: {
       folderName: "mboxStats",
       pathAbsOrRel: "" as string,
+      innerFiles: {
+        allMailList_csv: {
+          fileName: `allMailList${dotCsv}`,
+          pathAbsOrRel: "" as string,
+          freqMap: new Map<string, number>([]),
+        },
+      },
       innerFolders: {
         results: {
           folderName: "results",
-          pathAbsOrRel: "",
+          pathAbsOrRel: "" as string,
           innerFolders: {},
           innerFiles: {
             frequencySenderAddress: {
               fileName: `${str_frequency}${str_Sender}${str_Address}${dotCsv}`,
-              pathAbsOrRel: "",
+              pathAbsOrRel: "" as string,
+              freqMap: new Map<string, number>([]),
             },
             frequencySenderDomain: {
               fileName: `${str_frequency}${str_Sender}${str_Domain}${dotCsv}`,
-              pathAbsOrRel: "",
+              pathAbsOrRel: "" as string,
+              freqMap: new Map<string, number>([]),
             },
             frequencySenderFullInfo: {
               fileName: `${str_frequency}${str_Sender}${str_FullInfo}${dotCsv}`,
-              pathAbsOrRel: "",
+              pathAbsOrRel: "" as string,
+              freqMap: new Map<string, number>([]),
             },
 
             // not just first receiver, but includes all receivers. So, count of receivers can me more than count of senders
 
             frequencyReceiverAddress: {
               fileName: `${str_frequency}${str_Receiver}${str_Address}${dotCsv}`,
-              pathAbsOrRel: "",
+              pathAbsOrRel: "" as string,
+              freqMap: new Map<string, number>([]),
             },
             frequencyReceiverDomain: {
               fileName: `${str_frequency}${str_Receiver}${str_Domain}${dotCsv}`,
-              pathAbsOrRel: "",
+              pathAbsOrRel: "" as string,
+              freqMap: new Map<string, number>([]),
             },
             frequencyReceiverFullInfo: {
               fileName: `${str_frequency}${str_Receiver}${str_FullInfo}${dotCsv}`,
-              pathAbsOrRel: "",
+              pathAbsOrRel: "" as string,
+              freqMap: new Map<string, number>([]),
             },
           },
         },
       },
-      innerFiles: {
-        allMailList_csv: {
-          fileName: `allMailList${dotCsv}`,
-          pathAbsOrRel: "",
-        },
-      },
     },
   },
-  innerFiles: {},
 } as const;
 
 type TyCheckThatTObjExtendsTSource<TSource, TObj extends TSource> = TObj &
@@ -99,8 +107,8 @@ export const prepareOutputFolderStructure = (mboxFilePath: string) => {
     groundFolder.innerFolders.mboxStats.folderName,
   );
 
-  console.log("haaaa_end", process.argv, process.env.npm_config_sss); // npm run go --aaabbbrt="sdfs"
-  console.log("imushavaa", mboxFilePath, mboxStatsFolderPath);
+  // console.log("haaaa_end", process.argv, process.env.npm_config_sss); // npm run go --aaabbbrt="sdfs"
+  // console.log("imushavaa", mboxFilePath, mboxStatsFolderPath);
 
   // create mboxStats Folder
   try {
@@ -122,7 +130,7 @@ export const prepareOutputFolderStructure = (mboxFilePath: string) => {
 
   // create main list csv file
   try {
-    writeFileSync(allMailListFilePath, "Not used yet");
+    writeFileSync(allMailListFilePath, "");
 
     groundFolder.innerFolders.mboxStats.innerFiles.allMailList_csv[
       // @ts-ignore
@@ -160,17 +168,17 @@ export const prepareOutputFolderStructure = (mboxFilePath: string) => {
 
   //
 
-  const arrOfCandFiles = Object.keys(
+  const arrOfObjKeysOfCandFiles = Object.keys(
     resultsFoldersInnerFiles,
   ) as (keyof typeof resultsFoldersInnerFiles)[];
 
-  for (const propName of arrOfCandFiles) {
+  for (const propName of arrOfObjKeysOfCandFiles) {
     const thePath = path.join(
       resultsFolderPath,
       resultsFoldersInnerFiles[propName].fileName,
     );
     try {
-      writeFileSync(thePath, "Not used yet");
+      writeFileSync(thePath, "");
 
       resultsFoldersInnerFiles[propName][
         // @ts-ignore
