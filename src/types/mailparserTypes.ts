d61e3167c04a66c40d56b5a9740cd02d895d2c69
Levan Katsadze <leodevbro@gmail.com>
2024-03-02 20:22:29 +0400

    update mailparserTypes.ts file

--- src/types/mailparserTypes.ts
+++ src/types/mailparserTypes.ts
@@ -1,63 +1,54 @@
-import { Headers as TyMailparserHeaders } from "mailparser";
-
 export type TyMailDomain = `@${string}.${string}`;
 export type TyMailAddress = `${string}${TyMailDomain}`;
 
-const exampleOfOneValueOfFromOrTo = {
-  address: "leodevbro@gmail.com" as TyMailAddress | undefined,
-  name: "Levan Katsadze" as string,
-  group: [] as any[] | undefined,
-} as const;
+export type TyParticipant = {
+  address?: TyMailAddress; // "leodevbro@gmail.com"
+  name?: string; // "Levan Katsadze"
+  group?: any[];
+};
 
-const exampleOfGroupOfParticipants = {
-  value: [
-    exampleOfOneValueOfFromOrTo,
-  ] as (typeof exampleOfOneValueOfFromOrTo)[],
-  html: '<span class="mp_address_group"><span class="mp_address_name">Levan Katsadze</span> &lt;<a href="mailto:leodevbro@gmail.com" class="mp_address_email">leodevbro@gmail.com</a>&gt;</span>' as string,
-  text: '"Levan Katsadze" <leodevbro@gmail.com>' as string,
-} as const;
+export type TyParticipationFamilyInfo = {
+  html: string; // '<span class="mp_address_group"><span class="mp_address_name">Levan Katsadze</span> &lt;<a href="mailto:leodevbro@gmail.com" class="mp_address_email">leodevbro@gmail.com</a>&gt;</span>'
+  text: string; // generalInfo // '"Levan Katsadze" <leodevbro@gmail.com>'
+  value?: TyParticipant[];
+};
 
-export type TyGroupOfParticipants =
-  | typeof exampleOfGroupOfParticipants
-  | undefined;
+export type TyGmailMailHeadersAsObj = {
+  "x-gm-thrid"?: string; // "1786467213154837659"
+  "x-gmail-labels"?: string; // "Sent,Opened"
+  "mime-version"?: string; // "1.0"
+  references?: string[]; // ["<CAKQNcJcvLk-8O0X36+v1xQW3AzMgaKP9o_6XPMdzvfO+jeKsnw@mail.gmail.com>", "<C01B40B9-2E0B-4EF5-BC80-7CD20E769218@gmail.com>", ]
+  "in-reply-to"?: string; // "<C01B40B9-2E0B-4EF5-BC80-7CD20E769218@gmail.com>"
+  "content-type"?: {
+    value: string; // "multipart/alternative"
+    params: {
+      boundary: string; // "000000000000100d3c060dafb0ca"
+    };
+  };
 
-const exampleObjOfHeaders = {
-  "x-gm-thrid": "1786467213154837659" as string,
-  "x-gmail-labels": "Sent,Opened" as string,
-  "mime-version": "1.0" as string,
-  date: new Date(), // Date object which can be converted to this format string: "2023-12-30T01:01:24.000Z",
-  references: [
-    "<CAKQNcJcvLk-8O0X36+v1xQW3AzMgaKP9o_6XPMdzvfO+jeKsnw@mail.gmail.com>",
-    "<C01B40B9-2E0B-4EF5-BC80-7CD20E769218@gmail.com>",
-  ] as string[],
-  "in-reply-to": "<C01B40B9-2E0B-4EF5-BC80-7CD20E769218@gmail.com>" as string,
-  "message-id":
-    "<CAKQNcJf6xnUS+B4jEgnQr1psUXWXZbsH-G2f9T8d01XO8FrfvQ@mail.gmail.com>" as string,
+  subject?: string; // "Re: Find the most frequent sender - gmail"
 
-  subject: "Re: Find the most sender - gmail" as string,
   //
-  from: exampleOfGroupOfParticipants as TyGroupOfParticipants,
-  to: exampleOfGroupOfParticipants as TyGroupOfParticipants,
-  "delivered-to": exampleOfGroupOfParticipants as TyGroupOfParticipants,
-  cc: exampleOfGroupOfParticipants as TyGroupOfParticipants,
-  bcc: exampleOfGroupOfParticipants as TyGroupOfParticipants,
-  //
-  "content-type": {
-    value: "multipart/alternative" as string,
-    params: { boundary: "000000000000100d3c060dafb0ca" as string },
-  },
-} as const;
 
-export type TyExampleObjOfHeaders = typeof exampleObjOfHeaders;
-export type TyKeyOfMboxMailHeaders = keyof TyExampleObjOfHeaders;
+  "message-id": `${"<"}${string}${">"}`; // "<CAKQNcJf6xnUS+B4jEgnQr1psUXWXZbsH-G2f9T8d01XO8FrfvQ@mail.gmail.com>"
+  date?: Date; // Date object which can be converted to ISO format string: "2023-12-30T01:01:24.000Z",
+  //
+  from?: TyParticipationFamilyInfo;
+  to?: TyParticipationFamilyInfo;
+  "delivered-to"?: TyParticipationFamilyInfo;
+  cc?: TyParticipationFamilyInfo;
+  bcc?: TyParticipationFamilyInfo;
+};
+
+export type TyKeyOfMboxMailHeaders = keyof TyGmailMailHeadersAsObj;
 
 export type TyValueOfMboxMailHeaders =
-  TyExampleObjOfHeaders[TyKeyOfMboxMailHeaders];
+  TyGmailMailHeadersAsObj[TyKeyOfMboxMailHeaders];
 
 type TyDeepHintsOfHeadersMap = {
   get: <
     TKey extends TyKeyOfMboxMailHeaders,
-    TVal = TyExampleObjOfHeaders[TKey],
+    TVal = TyGmailMailHeadersAsObj[TKey],
   >(
     key: TKey,
   ) => TVal;
@@ -66,22 +57,30 @@ type TyDeepHintsOfHeadersMap = {
 export type TyMboxMailHeaders = TyDeepHintsOfHeadersMap &
   Map<TyKeyOfMboxMailHeaders, TyValueOfMboxMailHeaders>;
 
-// just to check
-// @ts-ignore
-type TyMailHeaders = TyMboxMailHeaders extends TyMailparserHeaders
-  ? TyMboxMailHeaders
-  : never;
-
 export type TyMainInfoForMail = {
-  from: TyExampleObjOfHeaders["from"];
-  to: TyExampleObjOfHeaders["to"];
-  "delivered-to": TyExampleObjOfHeaders["delivered-to"];
-  cc: TyExampleObjOfHeaders["cc"];
-  bcc: TyExampleObjOfHeaders["bcc"];
-  date: TyExampleObjOfHeaders["date"];
-  messageId: TyExampleObjOfHeaders["message-id"];
+  from: TyParticipationFamilyInfo;
+  to: TyParticipationFamilyInfo;
+  "delivered-to": TyParticipationFamilyInfo;
+  cc: TyParticipationFamilyInfo;
+  bcc: TyParticipationFamilyInfo;
+  //
+  date: TyGmailMailHeadersAsObj["date"];
+  messageId: TyGmailMailHeadersAsObj["message-id"];
 };
 
-// test:
-// const dsfsdf = new Map() as TyMboxMailHeaders;
-// export const qqqqq = dsfsdf.get("from");
+// zen/Zen means normalized, simplified, cleaned, to avoid runtime errors
+export type TyZenParticipant = {
+  address: string; // "leodevbro@gmail.com" | "" (or "emptyAddress" for frequency stats)
+  name: string; // "Levan Katsadze" | "" (or "emptyName" for frequency stats)
+};
+
+// zen/Zen means normalized, simplified, cleaned, to avoid runtime errors
+export type TyZenMainInfoForMail = {
+  from: TyZenParticipant[];
+  zenTo: TyZenParticipant[]; // combination of "to" and "delivered-to"
+  cc: TyZenParticipant[];
+  bcc: TyZenParticipant[];
+  //
+  date: TyGmailMailHeadersAsObj["date"];
+  messageId: TyGmailMailHeadersAsObj["message-id"];
+};
