3ffd8752711d2221f291779523f23704cf4c641b
Levan Katsadze <leodevbro@gmail.com>
2024-03-30 15:59:55 +0400

    update constants

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,8 @@
 export const str = {
+  authoringText: `gmail-mbox-stats v1.0.8
+Created by leodevbro (Levan Katsadze)
+--- linkedin.com/in/leodevbro
+--- github.com/leodevbro`,
   EMPTY: "(-)",
   STRANGE: "STRANGE-->",
 } as const;
